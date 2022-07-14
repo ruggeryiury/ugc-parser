f3a9294fb9ab35521ba0ed50efb3baf2f507b102
Ruggery Iury <86865266+ruggeryiury@users.noreply.github.com>
2022-07-14 01:49:39 -0300

    Update ugc2json.py

--- ugc2json.py
+++ ugc2json.py
@@ -1,5 +1,5 @@
 # UGC 2 JSON CONVERTER
-# Version 1.1
+# Version 1.2
 #
 # Created by Ruggery Iury Corrêa
 # GitHub: https://github.com/ruggeryiury
@@ -8,13 +8,18 @@
 # UGC to JSON Converter GitHub Repository
 # https://github.com/ruggeryiury/ugc2json_converter
 
-
 import json
 import os
 
+
+
+# Change this 'subdirs' variable to False if you want
+# UGC files from subdirectories to be converted
+subdirs = True
+
+
+
 script_path: str = os.path.dirname(__file__)
-
-
 def type_key_value(key_value: str) -> str | int | float | bool:
     new_value: str | int | float | bool
     try:
@@ -36,13 +41,12 @@ def type_key_value(key_value: str) -> str | int | float | bool:
     return new_value
 
 
-def ugc_parser(file_name: str) -> None:
+def ugc_parser(root: str, file_name: str) -> None:
     operator = []
     root_list = False
-    ugc_file = open(os.path.join(script_path, '{}.ugc'.format(
-        file_name)), 'r', encoding='utf-8').readlines()
-    new_file = open(os.path.join(
-        script_path, '{}.json'.format(file_name)), 'w', encoding='utf-8')
+    minify = False
+    ugc_file = open(os.path.join(root, '{}.ugc'.format(file_name)), 'r', encoding='utf-8').readlines()
+    new_file = open(os.path.join(root, '{}.json'.format(file_name)), 'w', encoding='utf-8')
     new_json = {}
     for line in ugc_file:
         operator_index: int = len(operator) - 1
@@ -65,6 +69,8 @@ def ugc_parser(file_name: str) -> None:
                 if key_value == 'list':
                     new_json = []
                     root_list = True
+                elif key_value == 'minify':
+                    minify = True
 
             # This will detect if line is declaring a new dict
             elif key_name == "#set":
@@ -100,22 +106,18 @@ def ugc_parser(file_name: str) -> None:
                             operator[operator_index]['content'][key_value] = {}
             elif key_name == "#endset":
                 if operator[operator_index - 1]['type'] == "LIST":
-                    operator[operator_index -
-                             1]['content'].append(operator[operator_index]['content'])
+                    operator[operator_index - 1]['content'].append(operator[operator_index]['content'])
                     operator.pop(operator_index)
                 else:
                     if operator_index == 0:
                         if root_list:
-                            new_json[len(new_json) -
-                                     1] = operator[operator_index]['content']
+                            new_json[len(new_json) - 1] = operator[operator_index]['content']
                             operator.pop(operator_index)
                         else:
-                            new_json[operator[operator_index]['name']
-                                     ] = operator[operator_index]['content']
+                            new_json[operator[operator_index]['name']] = operator[operator_index]['content']
                             operator.pop(operator_index)
                     else:
-                        operator[operator_index - 1]['content'][operator[operator_index]
-                                                                ['name']] = operator[operator_index]['content']
+                        operator[operator_index - 1]['content'][operator[operator_index]['name']] = operator[operator_index]['content']
                         operator.pop(operator_index)
 
             # This will detect if line is declaring a new list
@@ -153,26 +155,21 @@ def ugc_parser(file_name: str) -> None:
             elif key_name == "#endlist":
                 if operator_index == 0:
                     if root_list:
-                        new_json[len(new_json) -
-                                 1] = operator[operator_index]['content']
+                        new_json[len(new_json) - 1] = operator[operator_index]['content']
                         operator.pop(operator_index)
                     else:
-                        new_json[operator[operator_index]['name']
-                                 ] = operator[operator_index]['content']
+                        new_json[operator[operator_index]['name']] = operator[operator_index]['content']
                         operator.pop(operator_index)
                 else:
                     if operator[operator_index - 1]['type'] == "LIST":
-                        operator[operator_index -
-                                 1]['content'].append(operator[operator_index]['content'])
+                        operator[operator_index - 1]['content'].append(operator[operator_index]['content'])
                         operator.pop(operator_index)
                     else:
                         if operator_index == 0:
-                            new_json[operator[operator_index]['name']
-                                     ] = operator[operator_index]['content']
+                            new_json[operator[operator_index]['name']] = operator[operator_index]['content']
                             operator.pop(operator_index)
                         else:
-                            operator[operator_index - 1]['content'][operator[operator_index]
-                                                                    ['name']] = operator[operator_index]['content']
+                            operator[operator_index - 1]['content'][operator[operator_index]['name']] = operator[operator_index]['content']
                             operator.pop(operator_index)
 
             # Else (generally meaning this is a line with key/value entries)
@@ -191,14 +188,16 @@ def ugc_parser(file_name: str) -> None:
 
                     # Checking type
                     if operator[operator_index]['type'] == "SET":
-                        operator[operator_index]['content'][key_name] = type_key_value(
-                            key_value)
+                        operator[operator_index]['content'][key_name] = type_key_value(key_value)
                     elif operator[operator_index]['type'] == "LIST":
                         key_value = ' '.join(str(i) for i in line)
-                        operator[operator_index]['content'].append(
-                            type_key_value(key_value))
+                        operator[operator_index]['content'].append(type_key_value(key_value))
 
-    json.dump(new_json, new_file, ensure_ascii=False, indent=4)
+    if minify:
+        json.dump(new_json, new_file, ensure_ascii=False, separators=[',', ':'])
+    else:
+        json.dump(new_json, new_file, ensure_ascii=False, indent=4)
+
     new_file.close()
 
 
@@ -207,10 +206,19 @@ def init() -> None:
     Main function: UGC to JSON Converter.
     """
     for root, dirs, files in os.walk(script_path):
-        for file in files:
-            if file.endswith(".ugc"):
-                file_name = file[:-4]
-                ugc_parser(file_name)
+        if subdirs:
+            for file in files:
+                if file.endswith(".ugc"):
+                    file_name = file[:-4]
+                    ugc_parser(root, file_name)
+        else:
+            if root == script_path:
+                for file in files:
+                    if file.endswith(".ugc"):
+                        file_name = file[:-4]
+                        ugc_parser(root, file_name)
+            else:
+                pass
 
 
 init()
